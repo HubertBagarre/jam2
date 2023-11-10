c65121eb190cc45a33281a11681aa6df09d53ddf
hub <hubert.ramiandrasoa@gmaiL.com>
2023-11-10 23:56:25 +0100

    Kill zone, respawn and stocks

--- MagicalGirlFINALY/Assets/Scripts/Character.cs
+++ MagicalGirlFINALY/Assets/Scripts/Character.cs
@@ -12,10 +12,45 @@ public class Character : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int maxAirJumps = 2;
+    [SerializeField] private bool ledgeJumpIsAirJump = true;
+
+    private int jumpsLeft;
+    
+    //states
+    private State state = new State();
+
+    public static event Action<Character> OnCreated; 
+    public static event Action<Character> OnDeath; 
     
     private float targetSpeed = 0f;
     
     private Vector3 cachedVelocity;
+    
+    private class State
+    {
+        public bool grounded;
+        public bool stunned;
+        
+        public bool startup;
+        public bool attacking;
+        public bool recovering;
+        public bool CanInput => !stunned && !startup && !attacking && !recovering;
+        
+        public bool dead;
+    }
+    
+    private void Start()
+    {
+        InitStats();
+        
+        OnCreated?.Invoke(this);
+    }
+
+    public void InitStats()
+    {
+        jumpsLeft = maxAirJumps;
+    }
     
     public void Move(Vector2 direction)
     {
@@ -24,6 +59,10 @@ public class Character : MonoBehaviour
 
     public void Jump()
     {
+        if(jumpsLeft <= 0) return;
+        
+        if(!state.grounded) jumpsLeft--;
+        
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
@@ -42,5 +81,24 @@ public class Character : MonoBehaviour
         cachedVelocity = rb.velocity;
         cachedVelocity.x = targetSpeed;
         rb.velocity = cachedVelocity;
+    }
+
+    private void Kill()
+    {
+        OnDeath?.Invoke(this);
+        Debug.Log("OOF");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log($"{other.gameObject.name} entered trigger (layer {other.gameObject.layer})");
+        switch (other.gameObject.layer)
+        {
+            case 9:
+                Kill();
+                break;
+            default:
+                break;
+        }
     }
 }
