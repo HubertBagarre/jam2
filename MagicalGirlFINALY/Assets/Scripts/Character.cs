0f48700154c765a82add60bb9371f83750d8f208
esdragor <baudouinlapeze9@gmail.com>
2023-11-11 12:58:18 +0100

    Ledged

--- MagicalGirlFINALY/Assets/Scripts/Character.cs
+++ MagicalGirlFINALY/Assets/Scripts/Character.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Character : MonoBehaviour
 {
@@ -9,9 +11,11 @@ public class Character : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private FrameDataSo frameDataSo;
     [SerializeField] private List<GameObject> hitboxes;
+    [SerializeField,ReadOnly] private float gravityMultiplier = 1f;
     
     [Header("Settings")]
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float runSpeed = 5f;
+    [SerializeField] private float ledgeGravity = 0.3f;
     
     [Space]
     [SerializeField] private float groundRange = 0.1f;
@@ -23,7 +27,7 @@ public class Character : MonoBehaviour
     [SerializeField] private int maxAirJumps = 2;
     [SerializeField] private bool ledgeJumpIsAirJump = true;
     
-    [SerializeField] private int jumpsLeft;
+    [SerializeField,ReadOnly]  private int airJumpsLeft;
     
     [SerializeField] private float respawnInvulSeconds= 3;
     
@@ -47,6 +51,7 @@ public class Character : MonoBehaviour
     {
         public bool grounded;
         public bool dropping;
+        public bool ledged;
         
         public bool IsAttacking => totalFrames > 0;
         public int totalFrames => startup + active + recovering;
@@ -62,6 +67,22 @@ public class Character : MonoBehaviour
         public bool CanInput => !Stunned && !IsAttacking && !dead;
         
         public bool dead;
+
+        public void ResetStates()
+        {
+            grounded = false;
+            ledged = false;
+            dropping = false;
+            
+            maxStunDuration = 0;
+            stunDuration = 0;
+        
+            startup = 0;
+            active = 0;
+            recovering = 0;
+        
+            invulFrames = 0;
+        }
     }
     
     private void Start()
@@ -75,18 +96,12 @@ public class Character : MonoBehaviour
     
     public void InitStats()
     {
-        jumpsLeft = maxAirJumps;
+        airJumpsLeft = maxAirJumps;
         
+        gravityMultiplier = 1f;
         rb.velocity = Vector3.zero;
         
-        state.maxStunDuration = 0;
-        state.stunDuration = 0;
-        
-        state.startup = 0;
-        state.active = 0;
-        state.recovering = 0;
-        
-        state.invulFrames = 0;
+        state.ResetStates();
     }
 
     public void Respawn()
@@ -99,11 +114,15 @@ public class Character : MonoBehaviour
     public void Jump()
     {
         if(CannotInput) return;
-        
-        if(jumpsLeft <= 0) return;
-        
-        if(!state.grounded) jumpsLeft--;
 
+        var canJump = state.grounded || (state.ledged && !ledgeJumpIsAirJump);
+
+        if (!canJump) canJump = airJumpsLeft > 0;
+        
+        if(!canJump) return;
+        
+        airJumpsLeft--;
+        
         cachedVelocity = rb.velocity;
         cachedVelocity.y = 0;
         rb.velocity = cachedVelocity;
@@ -177,6 +196,39 @@ public class Character : MonoBehaviour
         DecreaseInvulFrames();
         Drop();
         CheckIsGrounded();
+        CheckLedging();
+    }
+
+    private void CheckLedging()
+    {
+       if(CannotInput || state.grounded) return;
+       
+       if(controller.StickInput.x == 0) return;
+       
+       var dir = (Vector3.right * controller.StickInput.x).normalized;
+       
+       var rayDist = groundRange+groundCheckHeight;
+       
+       var ledgeHit = Physics.Raycast(transform.position - Vector3.up * 0.5f + dir * ((1-groundCheckHeight)*0.5f), dir, out var hit,
+           rayDist,platformLayer);
+       if (!ledgeHit)
+           ledgeHit = Physics.Raycast(transform.position + Vector3.up * 0.5f + dir * ((1-groundCheckHeight)*0.5f), dir, out hit,
+               rayDist,platformLayer);
+       
+       Debug.DrawRay(transform.position - Vector3.up * 0.5f + dir * ((1-groundCheckHeight)*0.5f), dir * rayDist, Color.red); 
+       Debug.DrawRay(transform.position + Vector3.up * 0.5f + dir * ((1-groundCheckHeight)*0.5f), dir * rayDist, Color.red); 
+       
+       if(ledgeHit)
+       {
+           if (!state.ledged)
+           {
+               OnLedgeTouch();
+           }
+       }
+       else if(state.grounded)
+       {
+           OnAirborne();
+       }
     }
 
     private void Drop()
@@ -204,18 +256,15 @@ public class Character : MonoBehaviour
             groundHit = Physics.Raycast(transform.position + Vector3.right * 0.5f - Vector3.up * (1-groundCheckHeight), Vector3.down, out hit,
                 rayDist,mask);
         
-        Debug.DrawRay(transform.position - Vector3.right * 0.5f - Vector3.up * (1-groundCheckHeight), Vector3.down * rayDist, Color.red);
-        Debug.DrawRay(transform.position + Vector3.right * 0.5f - Vector3.up * (1-groundCheckHeight), Vector3.down * rayDist, Color.red);
-        
         if(Velocity.y > 0) groundHit = false;
         if(groundHit)
         {
             if (!state.grounded)
             {
-                OnTouchGround(hit.point);
+                OnTouchGround();
             }
         }
-        else if(state.grounded)
+        else if(state.grounded || state.ledged)
         {
             OnAirborne();
         }
@@ -233,8 +282,6 @@ public class Character : MonoBehaviour
         if(state.startup > 0) state.startup--;
         else if(state.active > 0) state.active--;
         else if(state.recovering > 0) state.recovering--;
-        
-        Debug.Log(state.totalFrames);
     }
 
     private void DecreaseStunDuration()
@@ -246,15 +293,21 @@ public class Character : MonoBehaviour
     private void FixedUpdate()
     {
         UpdateMove();
+        ApplyGravity();   
+    }
+
+    private void ApplyGravity()
+    {
+        rb.AddForce(Physics.gravity * (gravityMultiplier * rb.mass));
     }
 
     private void UpdateMove()
     {
         if(CannotInput) return;
-        if(controller == null) return;
+        if(state.ledged) return;
 
         cachedVelocity = rb.velocity;
-        cachedVelocity.x = controller.StickInput.x * speed;
+        cachedVelocity.x = controller.StickInput.x * runSpeed;
         rb.velocity = cachedVelocity;
     }
 
@@ -265,24 +318,30 @@ public class Character : MonoBehaviour
         
         OnDeath?.Invoke(this);
     }
-    
-    public void OnTouchGround(Vector3 groundPos)
+
+    public void OnLedgeTouch()
     {
-        jumpsLeft = maxAirJumps;
+        airJumpsLeft = maxAirJumps;
+        state.ledged = true;
+        gravityMultiplier = ledgeGravity;
+    }
+    
+    public void OnTouchGround()
+    {
+        airJumpsLeft = maxAirJumps;
         state.grounded = true;
-        rb.useGravity = false;
+        gravityMultiplier = 0f;
         
         var inverseVel = Velocity;
         inverseVel.y *= -1;
         rb.AddForce(inverseVel, ForceMode.VelocityChange);
-
-        var pos = transform.position;
     }
     
     public void OnAirborne()
     {
         state.grounded = false;
-        rb.useGravity = true;
+        state.ledged = false;
+        gravityMultiplier = 1f;
     }
 
     public void TakeHit(HitData data)
